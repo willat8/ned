1ecaa52f4a2e3f4240c946480b2c29944c432501
William Wilhelm <will@wilhelm.com.au>
2013-08-14 21:37:29 +1000

    Large code refactor

--- ned.py
+++ ned.py
@@ -1,7 +1,7 @@
 """This module provides tools for querying the NASA\IPAC Extragalactic Database (NED)
    and the Wide-field Infrared Survey Explorer (WISE) database."""
 
-import urllib, astropy.io.votable, time, warnings
+import urllib, astropy.io.votable, time, warnings, math
 
 NED_POSITION_SEARCH_PATH = "http://nedwww.ipac.caltech.edu/cgi-bin/nph-objsearch?of=xml_posn\
 &objname=%s"
@@ -12,94 +12,118 @@ WISE_SEARCH_PATH = "http://irsa.ipac.caltech.edu/cgi-bin/Gator/nph-query?catalog
 TWOMASS_SEARCH_PATH = "http://irsa.ipac.caltech.edu/cgi-bin/Gator/nph-query?catalog=fp_psc&outfmt=3\
 &objstr=%(lat).5f+%(lon).5f"
 
+class DataPoint:
+  """A storage class for frequency vs. flux data from various sources"""
+  def __init__(self, data):
+    # initialise default values with correct types for output string
+    self.index = 0
+    self.name = None
+    self.z = 0.
+    self.num = 0
+    self.freq = 0.
+    self.flux = 0.
+    self.source = None # refers to the data source
+    self.flag = 0
+    self.lat = 0.
+    self.lon = 0.
+    self.offset_from_ned = 0.
+    self.extinction = 0.
+    self.RM = None
+    self.RM_err = None
+    self.offset = None
+
+    [setattr(self, *entry) for entry in data.items()] # set proper values
+
+  def __repr__(self):
+    """Formats and prints the frequency vs flux data for a space-separated .dat file"""
+    return "%(index)d  %(name)s %(z).5f %(num)d   %(freq).3e %(flux).3e %(source)s  %(flag)c %(lat).5f %(lon).5f %(offset_from_ned).1f  %(extinction).3e  %(RM)s %(RM_err)s %(offset)s" % vars(self)
+
 class Source:
   """Instances of this class represent extragalactic objects"""
-  def __init__(self, data):
-    [setattr(self, *entry) for entry in data] # set instance vars
-    #the below are to be determined
-    self.lat = self.lon = None #from ned
-    self.flux = self.freq = self.uncertainty = self.units = self.flag = None #from ned (TO DO self.flag)
+  extinction = 1. # common for all sources?
+  def __init__(self, polarisation):
+    self.points = []
+    self.polarisation = polarisation
+    self.ned_position = None
+    self.ned_sed = None
     self.wise = None
     self.twomass = None
-    self.extinction = 1 #TO DO
-    self.offset = 0 #TO DO
 
-def get_ned_position_votable(source):
-  """Fetches and returns NED position data for a source, in an astropy votable.
-     Depends on NED name."""
-  xml_file = urllib.urlopen(NED_POSITION_SEARCH_PATH % source.ned_name.replace(" ","+")) #grab xml file-like-object from ned
-  with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    return astropy.io.votable.parse_single_table(xml_file) #parse xml to astropy votable
+    # common for all data points for this source
+    self.name = None
+    self.ned_lat = 0.
+    self.ned_lon = 0.
+    self.z = 0.
+    self.RM = None
+    self.RM_err = None
 
-def get_ned_sed_votable(source):
-  """Fetches and returns NED spectral energy distribution (SED) data for a source, in an astropy votable.
-     Depends on NED name."""
-  xml_file = urllib.urlopen(NED_SED_SEARCH_PATH % source.ned_name.replace(" ","+")) #grab xml file-like-object from ned
-  with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    return astropy.io.votable.parse_single_table(xml_file) #parse xml to astropy votable
+    self.parse_polarisation()
 
-def get_wise_votable(source):
-  """Fetches and returns WISE All-Sky Source Catalog data for a source, in an astropy votable.
-     Depends on NED latitude/longitude."""
-  xml_file = urllib.urlopen(WISE_SEARCH_PATH % {"lat": round(source.lat, 5), "lon": round(source.lon, 5)}) #grab xml file-like object from wise (note the position rounding)
-  with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    return astropy.io.votable.parse_single_table(xml_file)
+  def __repr__(self):
+    return "\n".join(map(repr, self.points))
 
-def get_twomass_votable(source):
-  """Fetches and returns 2MASS All-Sky Point Source Catalog data for a source, in an astropy votable.
-     Depends on NED latitude/longitude."""
-  xml_file = urllib.urlopen(TWOMASS_SEARCH_PATH % {"lat": round(source.lat, 5), "lon": round(source.lon, 5)}) #grab xml file-like object from 2mass (note the position rounding)
-  with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    return astropy.io.votable.parse_single_table(xml_file)
+  def parse_polarisation(self):
+    """Picks out the redshift and rotation measure from the polarisation data and records them"""
+    try:
+      data = dict(zip(("RA","dec","ned_name","z","RM","RM_err"), self.polarisation.split(",")))
+      self.name = data["ned_name"]
+      self.z = data["z"]
+      self.RM = data["RM"]
+      self.RM_err = data["RM_err"]
+    except: raise Exception("Can't find raw polarisation data!")
 
-def store_ned_position_data(source, votable):
-  """Picks out the J2000.0 equatorial latitude/longitude (decimal degrees) and records them"""
-  for key, name in [("lat", "pos_ra_equ_J2000_d"), ("lon", "pos_dec_equ_J2000_d")]:
-    try: setattr(source, key, votable.array[name].data.item()) #if no results found will error
-    except: continue
+  def parse_ned_position(self):
+    """Picks out the J2000.0 equatorial latitude/longitude (decimal degrees) and records them"""
+    try:
+      for key, name in [("ned_lat", "pos_ra_equ_J2000_d"), ("ned_lon", "pos_dec_equ_J2000_d")]:
+        setattr(self, key, self.ned_position.array[name].data.item())
+    except: raise Exception("Can't find raw NED position data!")
 
-def store_ned_sed_data(source, votable):
-  """Picks out the frequency and flux data and records them as lists"""
-  for key, name in [("freq", "Frequency"), ("flux", "NED Photometry Measurement"), ("uncertainty", "NED Uncertainty"), ("units", "NED Units")]:
-    try: setattr(source, key, votable.array[name].data.tolist()) #if no results found will error
-    except: continue
-  source.flag = "a" #TO DO
+  def parse_ned_sed(self):
+    """Picks out the frequency vs flux data and records them as data points"""
+    try:
+      [self.points.append(DataPoint({"index": 0, "name": self.name.replace(" ",""), "z": float(self.z), "num": 0, "freq": freq, "flux": flux, "source": "NED", "flag": 'a', "lat": self.ned_lat, "lon": self.ned_lon, "offset_from_ned": 0., "extinction": self.extinction, "RM": self.RM, "RM_err": self.RM_err, "offset": "0"})) for freq, flux in zip(map(float, self.ned_sed.array["Frequency"].data.tolist()), map(float, self.ned_sed.array["NED Photometry Measurement"].data.tolist()))]
+    except: raise Exception("Can't find raw NED SED data!")
 
-def store_wise_data(source, votable):
-  """Stores all the WISE data in a tuple"""
-  try: source.wise = votable.array.item() #if no results found will error
-  except: pass
+  def parse_wise(self):
+    try:
+      pass
+    except: raise Exception("Can't find raw WISE data!")
 
-def store_twomass_data(source, votable):
-  """Stores all the 2MASS data in a tuple"""
-  try: source.twomass = votable.array.item() #if no results found will error
-  except: pass
+  def parse_twomass(self):
+    try:
+      pass
+    except: raise Exception("Can't find raw 2MASS data!")
 
-def output_dat(index, source):
-  """Formats and prints the NED data for a space-separated .dat file"""
-  for num, (freq, flux) in enumerate(zip(source.freq, source.flux)):
-    print "%(index)d  %(name)s %(z).5f %(num)d   %(freq).3e %(flux).3e %(source)s  %(flag)c %(lat).5f %(lon).5f  %(extinction).3e  %(RM)s %(RM_err)s %(offset)s" % {"index": index, "name": source.ned_name.replace(" ",""), "z": float(source.z), "num": num+1, "freq": freq, "flux": flux, "source": "NED", "flag": source.flag, "lat": source.lat, "lon": source.lon, "extinction": source.extinction, "RM": source.RM, "RM_err": source.RM_err, "offset": source.offset}
-  
+def get_votable(url):
+  """Fetches from the web and returns data for a source, in an astropy votable"""
+  print url
+  try:
+    xml_file = urllib.urlopen(url) # grab xml file-like-object from the web
+    time.sleep(1) # respect request throttling recommendations
+    with warnings.catch_warnings():
+      warnings.simplefilter("ignore") # suppress astropy warnings
+      return astropy.io.votable.parse_single_table(xml_file) # parse xml to astropy votable
+  except: raise Exception("Could not download or interpret data. Are you connected to the Internet?")
+
+#.###############################################.#
 
 # temporary test data
-_test_source_data_list = [
+_test_polarisation_data = [
 "1.59417,-0.07364,FBQS J0006-0004,1.037,22.5,3.3",
-"197.16317,-9.84211,PKS 1306-09,0.46685,-27.3,2.0"]
+"197.16317,-9.84211,PKS 1306-09,0.46685,-27.3,2.0"
+]
 
-_sources = []
-[_sources.append(Source(zip(("RA","dec","ned_name","z","RM","RM_err"), _test_source_data.split(",")))) for _test_source_data in _test_source_data_list]
+_sources = [Source(entry) for entry in _test_polarisation_data]
+[setattr(_source, "ned_position", get_votable(NED_POSITION_SEARCH_PATH % _source.name.replace(" ","+"))) for _source in _sources] # fetch ned position data
+[setattr(_source, "ned_sed", get_votable(NED_SED_SEARCH_PATH % _source.name.replace(" ","+"))) for _source in _sources] # fetch ned sed data
+[_source.parse_ned_position() for _source in _sources] # parse and store ned position data
+[setattr(_source, "wise", get_votable(WISE_SEARCH_PATH % {"lat": _source.ned_lat, "lon": _source.ned_lon})) for _source in _sources] # fetch wise data
+[setattr(_source, "twomass", get_votable(TWOMASS_SEARCH_PATH % {"lat": _source.ned_lat, "lon": _source.ned_lon})) for _source in _sources] # fetch 2mass data
+
+[_source.parse_ned_sed() for _source in _sources] # parse and store ned sed data
+[_source.parse_wise() for _source in _sources] # parse and store wise data
+[_source.parse_twomass() for _source in _sources] # parse and store 2mass data
 
 # let's see if it works
-for index, _source in enumerate(_sources):
-  store_ned_position_data(_source, get_ned_position_votable(_source))
-  store_wise_data(_source, get_wise_votable(_source))
-  time.sleep(1)
-  store_ned_sed_data(_source, get_ned_sed_votable(_source))
-  if _source.wise is None:
-    store_twomass_data(_source, get_twomass_votable(_source))
-  time.sleep(1)
-  output_dat(index+1, _source)
+for _source in _sources: print _source
