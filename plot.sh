e83e110d1a1daadaee3d8c42ee50416700575ff1
William Wilhelm <will@wilhelm.com.au>
2013-08-25 10:31:52 +1000

    Added plotting shell script (gnuplot only)

---
+++ plot.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Pass in a plot-ready .dat file and gnuplot will generate a plot in postscript format.
+# Output plot will be stored in the same directory as the input .dat file.
+
+fullname="$1"
+filename=$(basename "$fullname")
+
+source_name="${filename%.*}"
+out_file="${fullname%.*}.ps"
+in_file="$fullname"
+
+gnuplot <<EOF
+  set term postscript enhanced color
+  set output "$out_file"
+
+  set logscale
+  set xrange[1e7:1e18]
+  set yrange[1e15:1e30]
+
+  set key autotitle columnhead
+  set xlabel "Rest-frame frequency, log_{10}({/Symbol-Oblique n}_{rest}) [Hz]"
+  set format x "%L"
+  set ylabel "Luminosity, log_{10}(L_{/Symbol-Oblique n}) [W Hz^{-1}]"
+  set format y "%L"
+  set title "$source_name"
+
+  plot for [col=2:5] "$in_file" using 1:col 
+EOF
